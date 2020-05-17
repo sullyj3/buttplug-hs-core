d1dae4fc09ff3df968c0388bb62856830752aeeb
James Sully <sullyj3@gmail.com>
2020-05-18 03:21:31 +1000

    Add vibePulse function

--- src/Buttplug/Extra.hs
+++ src/Buttplug/Extra.hs
@@ -6,8 +6,10 @@ import           Control.Monad.IO.Class
 import Buttplug
 
 vibePulse1s :: Int -> ButtPlugM ()
-vibePulse1s deviceIdx = do
-  vibrateOnlyMotor deviceIdx 1
-  liftIO $ threadDelay 1000000
-  vibrateOnlyMotor deviceIdx (1/255)
+vibePulse1s deviceIdx = vibePulse deviceIdx 1000000 1
 
+vibePulse :: Int -> Int -> Double -> ButtPlugM ()
+vibePulse deviceIdx microsnds speed = do
+  vibrateOnlyMotor deviceIdx speed
+  liftIO $ threadDelay microsnds
+  vibrateOnlyMotor deviceIdx (1/255) -- todo: change once Stop is fixed in server for youou
