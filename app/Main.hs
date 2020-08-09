dfeb850e220f5e3b70c021cc54d2846583730ae5
James Sully <sullyj3@gmail.com>
2020-08-09 18:49:58 +1000

    playing with main

--- app/Main.hs
+++ app/Main.hs
@@ -28,17 +28,52 @@ app = ButtPlugApp { handleDeviceAdded = handleDeviceAdded }
         Just (Dev.MessageAttributes _nMotors) -> vibeTease1 dev 1
         Nothing -> pure ()
 
+foo dev = do
+  rampdown5s dev
+  stopDevice dev
+
+randomInterp :: Device -> ButtPlugM ()
+randomInterp dev = do
+  start <- liftIO $ randomRIO (0, 1)
+  go start
+
+  where go :: Double -> ButtPlugM ()
+        go a = do
+          b <- liftIO $ randomRIO (0, 1)
+          duration <- liftIO $ randomRIO (0.1,1)
+          interpolate dev a b duration
+          go b
+
 vibeTease1 :: Device -> Double -> ButtPlugM ()
 vibeTease1 dev scale = do
   liftIO do putStrLn $ "Starting vibeTease1, scale=" <> show scale
             putStrLn ""
   forever (vibeRampUpPulse dev scale)
 
+tease2 :: Device -> ButtPlugM ()
+tease2 dev = for_ [1..] \n -> do
+  vibePulse dev (n*0.5) 1
+  waitLen <- liftIO $ randomRIO (5,15)
+  sleep waitLen
+
+fastThrob :: Device -> ButtPlugM ()
+fastThrob dev = do
+  forever $ vibeRampUp dev 1 0.1
+
+vibeRampUp :: Device -> Double -> Double -> ButtPlugM ()
+vibeRampUp dev scale periodS = do
+  
+  for_ [1..nSamples] \i ->
+    vibrateAllMotors dev (scale * i / nSamples) >> sleep (1/sampleRateHz)
+  stopDevice dev
+  where
+    nSamples = sampleRateHz * periodS
+
+sampleRateHz = 100
+
 vibeRampUpPulse :: Device -> Double -> ButtPlugM ()
 vibeRampUpPulse dev scale = do
-  for_ [1..50] \i ->
-    vibrateAllMotors dev (scale * i / 50) >> sleep 0.05
-  stopDevice dev
+  vibeRampUp dev scale 0.1
 
   pulses <- liftIO $ randomRIO (1, 15)
   for_ [1..pulses] \i -> do
@@ -46,6 +81,21 @@ vibeRampUpPulse dev scale = do
     stopDevice dev <* sleep (i * 0.03)
 
   sleep $ 0.3 + pulses * 0.065
+
+interpolate :: Device -> Double -> Double -> Double -> ButtPlugM ()
+interpolate dev a b duration = do
+  for_ [1..nSamples] \s -> do
+    let freq = lerp a b (s/nSamples)
+    vibrateAllMotors dev freq
+    sleep (1/sampleRateHz)
+  where
+    nSamples = sampleRateHz * duration
+
+rampUp5s dev = interpolate dev 0 1 5
+rampdown5s dev = interpolate dev 1 0 5
+
+lerp :: Double -> Double -> Double -> Double
+lerp a b t = a + t * (b - a)
 
 --------------------------------------------------------------------------------
 main :: IO ()
