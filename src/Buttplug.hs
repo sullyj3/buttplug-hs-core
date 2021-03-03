a5ac6cd73bd8baef93db7e4a285e94b4db27cc21
James Sully <sullyj3@gmail.com>
2021-03-03 17:07:44 +1100

    Expose public interface to Message

--- src/Buttplug.hs
+++ src/Buttplug.hs
@@ -1,6 +1,21 @@
-module Buttplug 
-  ( module Buttplug.Message
+module Buttplug
+  ( -- Message.hs
+    clientMessageVersion
+  , ErrorCode(..)
+  , RawData(..)
+  , Vibrate(..)
+  , Rotate(..)
+  , LinearActuate(..)
+  , Message(..)
+  , isServerInfo
+  , isOk
+  , isScanningFinished
+  , isDeviceList
+
+  -- Connector.hs
   , module Buttplug.Connector 
+  -- Device.hs
+
   ) where
 
 -- todo - decide what should be uservisible by default
