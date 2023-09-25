6a626f388948af8a4730c6fcc9379e976b7ecee1
piq9117 <piq9117@gmail.com>
2023-09-24 20:28:32 -0400

    replace deriving generic with manual derivation

--- src/Buttplug/Core/Message.hs
+++ src/Buttplug/Core/Message.hs
@@ -8,6 +8,7 @@
 {-# LANGUAGE BlockArguments         #-}
 {-# LANGUAGE DeriveGeneric          #-}
 {-# LANGUAGE LambdaCase             #-}
+{-# LANGUAGE RecordWildCards        #-}
 
 {- |
 Module      : Buttplug.Core.Message
@@ -22,23 +23,22 @@ Contains the Message type, representing Buttplug protocol messages
 -}
 module Buttplug.Core.Message where
 
-import           GHC.Generics
+import           Data.Foldable                ( asum )
 import           Data.Word                    ( Word32 )
 import           Data.Text                    ( Text )
 import           Data.ByteString              ( ByteString )
 import qualified Data.ByteString              as BS
 import           Data.Aeson                   ( ToJSON(..)
                                               , FromJSON(..)
-                                              , genericToJSON
-                                              , Options(..)
-                                              , SumEncoding(..)
-                                              , genericParseJSON 
+                                              , object
+                                              , (.=)
+                                              , withObject
+                                              , (.:)
                                               )
 import           Data.Map.Strict              ( Map )
 
-import qualified Buttplug.Core.Device              as Dev
-import           Buttplug.Core.Device              ( Device(..) )
-import           Buttplug.Core.Internal.JSONUtils
+import qualified Buttplug.Core.Device         as Dev
+import           Buttplug.Core.Device         ( Device(..) )
 
 
 -- | The version of the Buttplug message protocol that the client speaks.
@@ -56,7 +56,7 @@ data ErrorCode = ERROR_UNKNOWN  -- ^ An unknown error occurred.
                | ERROR_PING     -- ^ A ping was not sent in the expected time.
                | ERROR_MSG      -- ^ A message parsing or permission error occurred.
                | ERROR_DEVICE   -- ^ A command sent to a device returned an error.
-               deriving (Enum, Show, Eq, Generic)
+               deriving (Enum, Show, Eq)
 
 
 -- TODO these should probably convert with Word not Int
@@ -92,7 +92,7 @@ instance FromJSON ErrorCode where
 
 -- | Used for the Raw* messages.
 newtype RawData = RawData ByteString
-  deriving (Generic, Show, Eq)
+  deriving (Show, Eq)
 
 
 instance ToJSON RawData where
@@ -106,16 +106,20 @@ instance FromJSON RawData where
 data Vibrate = Vibrate { vibrateIndex :: Word32
                        , vibrateSpeed :: Double
                        }
-  deriving (Generic, Show, Eq)
+  deriving (Show, Eq)
 
 
 instance ToJSON Vibrate where
-  toJSON = genericToJSON (stripPrefixOptions "vibrate")
-
+  toJSON vibrate = object
+    [ "Index" .= vibrateIndex vibrate,
+      "Speed" .= vibrateSpeed vibrate
+    ]
 
 instance FromJSON Vibrate where
-  parseJSON = genericParseJSON (stripPrefixOptions "vibrate")
-
+  parseJSON = withObject "Vibrate" $ \o ->
+    Vibrate
+      <$> o .: "Index"
+      <*> o .: "Speed"
 
 -- | Used in RotateCmd to specify the speed and direction of rotation of the
 -- motor at the given index
@@ -124,15 +128,21 @@ data Rotate = Rotate
   , rotateSpeed :: Double
   , rotateClockwise :: Bool
   }
-  deriving (Generic, Show, Eq)
+  deriving (Show, Eq)
 
 instance ToJSON Rotate where
-  toJSON = genericToJSON pascalCaseOptions { fieldLabelModifier = stripPrefix "rotate" }
-
+  toJSON rotate = object
+    [ "Index" .= rotateIndex rotate,
+      "Speed" .= rotateSpeed rotate,
+      "Clockwise" .= rotateClockwise rotate
+    ]
 
 instance FromJSON Rotate where
-  parseJSON = genericParseJSON pascalCaseOptions { fieldLabelModifier = stripPrefix "rotate" }
-
+  parseJSON = withObject "Rotate" $ \o ->
+    Rotate
+      <$> o .: "Index"
+      <*> o .: "Speed"
+      <*> o .: "Clockwise"
 
 -- | Used in LinearCmd to specify how to move the linear actuator at the given
 -- index
@@ -141,16 +151,22 @@ data LinearActuate = LinearActuate
   , linActDuration :: Word32
   , linActPosition :: Double
   }
-  deriving (Generic, Show, Eq)
+  deriving (Show, Eq)
 
 
 instance ToJSON LinearActuate where
-  toJSON = genericToJSON pascalCaseOptions { fieldLabelModifier = stripPrefix "linAct" }
-
+  toJSON linearActuate = object
+    [ "Index" .= linActIndex linearActuate,
+      "Duration" .= linActDuration linearActuate,
+      "Position" .= linActPosition linearActuate
+    ]
 
 instance FromJSON LinearActuate where
-  parseJSON = genericParseJSON pascalCaseOptions { fieldLabelModifier = stripPrefix "linAct" }
-
+  parseJSON = withObject "LinearActuate" $ \o ->
+    LinearActuate
+      <$> o .: "Index"
+      <*> o .: "Duration"
+      <*> o .: "Position"
 
 -- TODO technically Ids should be Word32, since the maximum id is 4294967295.
 -- Not sure whether this applies to other unsigned fields, should find out
@@ -245,16 +261,266 @@ data Message =
                                    , msgDeviceIndex :: Word32
                                    , msgRSSILevel :: Int
                                    }
-  deriving (Show, Eq, Generic)
+  deriving (Show, Eq)
 
 
 instance ToJSON Message where
-  toJSON = genericToJSON $ pascalCaseOptions { sumEncoding = ObjectWithSingleField
-                                             , fieldLabelModifier = stripPrefix "msg"
-                                             , constructorTagModifier = stripPrefix "Msg" }
-
+  toJSON (MsgOk {..}) = object
+    [ "Ok" .= object
+      [ "Id" .= msgId
+      ]
+    ]
+  toJSON (MsgError {..}) = object
+    [ "Error" .= object
+      [ "Id" .= msgId
+      , "ErrorMessage" .= msgErrorMessage
+      , "ErrorCode" .= msgErrorCode
+      ]
+    ]
+  toJSON (MsgPing {..}) = object
+    [ "Ping" .= object
+      [ "Id" .= msgId
+      ]
+    ]
+  toJSON (MsgRequestServerInfo {..}) = object
+    [ "RequestServerInfo" .= object
+      [ "Id" .= msgId
+      , "ClientName" .= msgClientName
+      , "MessageVersion" .= msgMessageVersion
+      ]
+    ]
+  toJSON (MsgServerInfo {..}) = object
+    [ "ServerInfo" .= object
+      [ "Id" .= msgId
+      , "ServerName" .= msgServerName
+      , "MessageVersion" .= msgMessageVersion
+      , "MaxPingTime" .= msgMaxPingTime
+      ]
+    ]
+  toJSON (MsgStartScanning {..}) = object
+    [ "StartScanning" .= object
+      [ "Id" .= msgId
+      ]
+    ]
+  toJSON (MsgStopScanning {..}) = object
+    [ "StopScanning" .= object
+      [ "Id" .= msgId
+      ]
+    ]
+  toJSON (MsgScanningFinished {..}) = object
+    [ "ScanningFinished" .= object
+      [ "Id" .= msgId
+      ]
+    ]
+  toJSON (MsgRequestDeviceList {..}) = object
+    [ "RequestDeviceList" .= object
+      [ "Id" .= msgId
+      ]
+    ]
+  toJSON (MsgDeviceList {..}) = object
+    [ "DeviceList" .= object
+      [ "Id" .= msgId
+      , "Devices" .= msgDevices
+      ]
+    ]
+  toJSON (MsgDeviceAdded {..}) = object
+    [ "DeviceAdded" .= object
+      [ "Id" .= msgId
+      , "DeviceName" .= msgDeviceName
+      , "DeviceIndex" .= msgDeviceIndex
+      , "DeviceMessages" .= msgDeviceMessages
+      ]
+    ]
+  toJSON (MsgDeviceRemoved {..}) = object
+    [ "DeviceRemoved" .= object
+      [ "Id" .= msgId
+      , "DeviceIndex" .= msgDeviceIndex
+      ]
+    ]
+  toJSON (MsgRawWriteCmd {..}) = object
+    [ "RawWriteCmd" .= object
+      [ "Id" .= msgId
+      , "DeviceIndex" .= msgDeviceIndex
+      , "Endpoint" .= msgEndpoint
+      , "Data" .= msgData
+      , "WriteWithResponse" .= msgWriteWithResponse
+      ]
+    ]
+  toJSON (MsgRawReadCmd {..}) = object
+    [ "RawReadCmd" .= object
+      [ "Id" .= msgId
+      , "DeviceIndex" .= msgDeviceIndex
+      , "Endpoint" .= msgEndpoint
+      , "ExpectedLength" .= msgExpectedLength
+      , "WaitForData" .= msgWaitForData
+      ]
+    ]
+  toJSON (MsgRawReading {..}) = object
+    [ "RawReading" .= object
+      [ "Id" .= msgId
+      , "DeviceIndex" .= msgDeviceIndex
+      , "Endpoint" .= msgEndpoint
+      , "Data" .= msgData
+      ]
+    ]
+  toJSON (MsgRawSubscribeCmd {..}) = object
+    [ "RawSubscribeCmd" .= object
+      [ "Id" .= msgId
+      , "DeviceIndex" .= msgDeviceIndex
+      , "Endpoint" .= msgEndpoint
+      ]
+    ]
+  toJSON (MsgRawUnsubscribeCmd {..}) = object
+    [ "RawUnsubscribeCmd" .= object
+      [ "Id" .= msgId
+      , "DeviceIndex" .= msgDeviceIndex
+      , "Endpoint" .= msgEndpoint
+      ]
+    ]
+  toJSON (MsgStopDeviceCmd {..}) = object
+    [ "StopDeviceCmd" .= object
+      [ "Id" .= msgId
+      , "DeviceIndex" .= msgDeviceIndex
+      ]
+    ]
+  toJSON (MsgStopAllDevices {..}) = object
+    [ "StopAllDevices" .= object
+      [ "Id" .= msgId
+      ]
+    ]
+  toJSON (MsgVibrateCmd {..}) = object
+    [ "VibrateCmd" .= object
+      [ "Id" .= msgId
+      , "DeviceIndex" .= msgDeviceIndex
+      , "Speeds" .= msgSpeeds
+      ]
+    ]
+  toJSON (MsgLinearCmd {..}) = object
+    [ "LinearCmd" .= object
+      [ "Id" .= msgId
+      , "DeviceIndex" .= msgDeviceIndex
+      , "Vectors" .= msgVectors
+      ]
+    ]
+  toJSON (MsgRotateCmd {..}) = object
+    [ "RotateCmd" .= object
+      [ "Id" .= msgId
+      , "DeviceIndex" .= msgDeviceIndex
+      , "Rotations" .= msgRotations
+      ]
+    ]
+  toJSON (MsgBatteryLevelCmd {..}) = object
+    [ "BatteryLevelCmd" .= object
+      [ "Id" .= msgId
+      , "DeviceIndex" .= msgDeviceIndex
+      ]
+    ]
+  toJSON (MsgBatteryLevelReading {..}) = object
+    [ "BatteryLevelReading" .= object
+      [ "Id" .= msgId
+      , "DeviceIndex" .= msgDeviceIndex
+      , "BatteryLevel" .= msgBatteryLevel
+      ]
+    ]
+  toJSON (MsgRSSILevelCmd {..}) = object
+    [ "RSSILevelCmd" .= object
+      [ "Id" .= msgId
+      , "DeviceIndex" .= msgDeviceIndex
+      ]
+    ]
+  toJSON (MsgRSSILevelReading {..}) = object
+    [ "RSSILevelReading" .= object
+      [ "Id" .= msgId
+      , "DeviceIndex" .= msgDeviceIndex
+      , "RSSILevel" .= msgRSSILevel
+      ]
+    ]
 
 instance FromJSON Message where
-  parseJSON = genericParseJSON $ pascalCaseOptions { sumEncoding = ObjectWithSingleField
-                                                   , fieldLabelModifier = stripPrefix "msg"
-                                                   , constructorTagModifier = stripPrefix "Msg" }
+  parseJSON = withObject "Message" $ \o -> asum
+    [ MsgOk <$> (o .: "Ok" >>= (.: "Id")),
+      MsgError
+        <$> (o .: "Error" >>= (.: "Id"))
+        <*> (o .: "Error" >>= (.: "ErrorMessage"))
+        <*> (o .: "Error" >>= (.: "ErrorCode")),
+      MsgPing <$> (o .: "Ping" >>= (.: "Id")),
+      MsgRequestServerInfo
+        <$> (o .: "RequestServerInfo" >>= (.: "Id"))
+        <*> (o .: "RequestServerInfo" >>= (.: "ClientName"))
+        <*> (o .: "RequestServerInfo" >>= (.: "MessageVersion")),
+      MsgServerInfo
+        <$> (o .: "ServerInfo" >>= (.: "Id"))
+        <*> (o .: "ServerInfo" >>= (.: "ServerName"))
+        <*> (o .: "ServerInfo" >>= (.: "MessageVersion"))
+        <*> (o .: "ServerInfo" >>= (.: "MaxPingTime")),
+      MsgStartScanning  <$> (o .: "StartScanning" >>= (.: "Id")),
+      MsgStopScanning <$> (o .: "StopScanning" >>= (.: "Id")),
+      MsgScanningFinished  <$> (o .: "ScanningFinished" >>= (.: "Id")),
+      MsgRequestDeviceList <$> (o .: "RequestDeviceList" >>= (.: "Id")),
+      MsgDeviceList
+        <$> (o .: "DeviceList" >>= (.: "Id"))
+        <*> (o .: "DeviceList" >>= (.: "Devices")),
+      MsgDeviceAdded
+        <$> (o .: "DeviceAdded" >>= (.: "Id"))
+        <*> (o .: "DeviceAdded" >>= (.: "DeviceName"))
+        <*> (o .: "DeviceAdded" >>= (.: "DeviceIndex"))
+        <*> (o .: "DeviceAdded" >>= (.: "DeviceMessages")),
+      MsgDeviceRemoved
+        <$> (o .: "DeviceRemoved" >>= (.: "Id"))
+        <*> (o .: "DeviceRemoved" >>= (.: "DeviceIndex")),
+      MsgRawWriteCmd
+        <$> (o .: "RawWriteCmd" >>= (.: "Id"))
+        <*> (o .: "RawWriteCmd" >>= (.: "DeviceIndex"))
+        <*> (o .: "RawWriteCmd" >>= (.: "Endpoint"))
+        <*> (o .: "RawWriteCmd" >>= (.: "Data"))
+        <*> (o .: "RawWriteCmd" >>= (.: "WriteWithResponse")),
+      MsgRawReadCmd
+        <$> (o .: "RawReadCmd" >>= (.: "Id"))
+        <*> (o .: "RawReadCmd" >>= (.: "DeviceIndex"))
+        <*> (o .: "RawReadCmd" >>= (.: "Endpoint"))
+        <*> (o .: "RawReadCmd" >>= (.: "ExpectedLength"))
+        <*> (o .: "RawReadCmd" >>= (.: "WaitForData")),
+      MsgRawReading
+        <$> (o .: "RawReading" >>= (.: "Id"))
+        <*> (o .: "RawReading" >>= (.: "DeviceIndex"))
+        <*> (o .: "RawReading" >>= (.: "Endpoint"))
+        <*> (o .: "RawReading" >>= (.: "Data")),
+      MsgRawSubscribeCmd
+        <$> (o .: "RawSubscribeCmd" >>= (.: "Id"))
+        <*> (o .: "RawSubscribeCmd" >>= (.: "DeviceIndex"))
+        <*> (o .: "RawSubscribeCmd" >>= (.: "Endpoint")),
+      MsgRawUnsubscribeCmd
+        <$> (o .: "RawUnsubscribeCmd" >>= (.: "Id"))
+        <*> (o .: "RawUnsubscribeCmd" >>= (.: "DeviceIndex"))
+        <*> (o .: "RawUnsubscribeCmd" >>= (.: "Endpoint")),
+      MsgStopDeviceCmd
+        <$> (o .: "StopDeviceCmd" >>= (.: "Id"))
+        <*> (o .: "StopDeviceCmd" >>= (.: "DeviceIndex")),
+      MsgStopAllDevices <$> (o .: "StopAllDevices" >>= (.: "Id")),
+      MsgVibrateCmd
+        <$> (o .: "VibrateCmd" >>= (.: "Id"))
+        <*> (o .: "VibrateCmd" >>= (.: "DeviceIndex"))
+        <*> (o .: "VibrateCmd" >>= (.: "Speeds")),
+      MsgLinearCmd
+        <$> (o .: "LinearCmd" >>= (.: "Id"))
+        <*> (o .: "LinearCmd" >>= (.: "DeviceIndex"))
+        <*> (o .: "LinearCmd" >>= (.: "Vectors")),
+      MsgRotateCmd
+        <$> (o .: "RotateCmd" >>= (.: "Id"))
+        <*> (o .: "RotateCmd" >>= (.: "DeviceIndex"))
+        <*> (o .: "RotateCmd" >>= (.: "Rotations")),
+      MsgBatteryLevelCmd
+        <$> (o .: "BatteryLevelCmd" >>= (.: "Id"))
+        <*> (o .: "BatteryLevelCmd" >>= (.: "DeviceIndex")),
+      MsgBatteryLevelReading
+        <$> (o .: "BatteryLevelReading" >>= (.: "Id"))
+        <*> (o .: "BatteryLevelReading" >>= (.: "DeviceIndex"))
+        <*> (o .: "BatteryLevelReading" >>= (.: "BatteryLevel")),
+      MsgRSSILevelCmd
+        <$> (o .: "RSSILevelCmd" >>= (.: "Id"))
+        <*> (o .: "RSSILevelCmd" >>= (.: "DeviceIndex")),
+      MsgRSSILevelReading
+        <$> (o .: "RSSILevelReading" >>= (.: "Id"))
+        <*> (o .: "RSSILevelReading" >>= (.: "DeviceIndex"))
+        <*> (o .: "RSSILevelReading" >>= (.: "RSSILevel"))
+    ]
